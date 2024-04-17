103a0a1ebd88e14aa0db94f614a9fbf5daf79987
Rafif Firmansyah <rafif.firmansyah@gmail.com>
2024-04-17 16:49:00 +0700

    Implement unsubscribe function in Notification service.

--- src/service/notification.rs
+++ src/service/notification.rs
@@ -7,13 +7,28 @@ use crate::model::product::Product;
 use crate::model::subscriber::Subscriber;
 use crate::repository::subscriber::SubscriberRepository;
 
+use super::product;
+
 pub struct NotificationService;
 
 impl NotificationService{
-    pub fn subscriber(product_type: &str, subscriber: Subscriber) -> Result<Subscriber> {
+    pub fn subscribe(product_type: &str, subscriber: Subscriber) -> Result<Subscriber> {
         let product_type_upper: String = product_type.to_uppercase();
         let product_type_str: &str = product_type_upper.as_str();
         let subscriber_result: Subscriber = SubscriberRepository::add(product_type_str, subscriber);
         return Ok(subscriber_result);
+    }
+
+    pub fn unsubscribe(product_type: &str, url: &str) -> Result<Subscriber> {
+        let product_type_upper: String = product_type.to_uppercase();
+        let product_type_str: &str = product_type_upper.as_str();
+        let result: Option<Subscriber> = SubscriberRepository::delete(product_type_str, url);
+        if result.is_none() {
+            return Err(compose_error_response(
+                Status::NotFound,
+                String::from("Subscriber not found.")
+            ));
+        }
+        return Ok(result.unwrap())
     }
 }
